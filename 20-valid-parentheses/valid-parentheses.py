37e86df73c976f5ca64046eeb9005a6807344de5
duman <100424533+adumannn@users.noreply.github.com>
2025-10-10 10:44:32 +0500

    Time: 0 ms (100.00%) | Memory: 18.1 MB (14.53%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s):
+        stack = []
+        matching = {"(": ")", "{": "}", "[": "]"}
+
+        for c in s:
+            if c in matching:
+                stack.append(c)
+            else:
+                if not stack:
+                    return False
+
+                previous_opening = stack.pop()
+                if matching[previous_opening] != c:
+                    return False
+
+        return not stack
