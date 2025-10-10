88d17d8ab0e8c54fe0b915df696fbfe068a7c3b0
duman <100424533+adumannn@users.noreply.github.com>
2025-10-10 10:17:39 +0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (71.27%) - LeetSync

---
+++ 2384-root-equals-sum-of-children/root-equals-sum-of-children.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def checkTree(self, root: Optional[TreeNode]) -> bool:
+        left = root.left.val
+        right = root.right.val
+        cnt = left + right
+        if cnt == root.val:
+            return True
+        else:
+            return False
+
+        
