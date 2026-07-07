6771c61d42b94d0da7c5130f28a61dbac88d3333
duman <100424533+adumannn@users.noreply.github.com>
2026-07-07 13:38:05 +0500

    Time: 0 ms (100.00%) | Memory: 19.5 MB (14.68%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.py
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.py
@@ -3,16 +3,19 @@ class Solution:
 
         if n == 0:
             return 0
-
-        n = str(n)
-
-        ans = 0
-        x = ''
-
-        for nums in n:
-            if nums != '0':
-                x += nums
-                ans += int(nums)
         
-        return int(x) * ans
+        s = res = 0
+        m = 1
+
+        while (n > 0):
+            d = n % 10
+
+            if d != 0:
+                res = d * m + res
+                m *= 10
+                s += d
+            
+            n //= 10
+        
+        return res * s
         
